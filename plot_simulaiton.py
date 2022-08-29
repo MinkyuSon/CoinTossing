6d07eb2ea60e969b7dd1a47c78ac8a8070dab5ae
Minkyu Son <mkyu.son@gmail.com>
2022-08-29 18:01:31 +0000

    new plot

--- plot_simulaiton.py
+++ plot_simulaiton.py
@@ -9,7 +9,11 @@ avgPerNs = [
         [0.4018486612383934,0.41330665099425146,0.4160149920742702,0.4166666666666667],
         [0.0561675346135595,0.07699905917240113,0.08214570168004924,0.08333333333333333]
         ]
-
+yRanges = [
+        [0.22,0.255],
+        [0.4, 0.42],
+        [0.05,0.087]
+        ]
 plt.rcParams["figure.figsize"] = (12,4)
 fig, axes = plt.subplots(1,3)
 
@@ -22,11 +26,14 @@ for k in range(3):
     title = titles[k]
     avgPerN = avgPerNs[k]
     ds = h5py.File(title+'_simulation_data.hdf5','r')
+    yRange = yRanges[k]
     for j in range(3):
         n = ns[j]
         d = ds[str(n)]
-        ax.plot(np.log10(np.arange(10001,d.shape[0]+1)), d[10000:]/np.arange(10001,d.shape[0]+1)/n, linewidth=0.5, label='n={n}'.format(n=n))
+        avg = d/np.arange(1,d.shape[0]+1)/n
+        ax.plot(np.log10(np.arange(10001,d.shape[0]+1))[::10], avg[10000::10], linewidth=0.5, label='n={n}'.format(n=n))
         ax.axhline(y=avgPerN[j], color='r', linestyle='--', linewidth=1)
+        ax.set_ylim((min(yRange[0],avg.min()),max(yRange[1],avg.max())))
     ax.axhline(y=avgPerN[-1], color='m', linestyle='--', linewidth=1)
     ax.set_title(title)
     ax.legend()
